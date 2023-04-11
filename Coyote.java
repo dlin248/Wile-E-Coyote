318df9ae40a81f357fe03489819ad24789bed032
dlin248 <122913408+dlin248@users.noreply.github.com>
2023-04-11 00:11:11 -0700

    Add files via upload

--- Coyote.java
+++ Coyote.java
@@ -1,4 +1,10 @@
 import info.gridworld.actor.Critter;
+import info.gridworld.grid.Grid;
+import info.gridworld.actor.Actor;
+import info.gridworld.grid.Location;
+
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * woof woof woof
@@ -10,8 +16,82 @@ import info.gridworld.actor.Critter;
  
 public class Coyote extends Critter
 {
+	private int steps;
+	private boolean end;
+	private int sleep;
 	public Coyote()
 	{
+		setColor(null);
+		end = false;
+		sleep = 0;
+		int direction = (int)(Math.random() * 8)*45;
+		setDirection(direction);
+		steps = 0;
+	}
+	/**
+	 * Overrides the act method in Critter 
+	 */
+    public void act()
+    {
+        if (getGrid() == null)
+            return;
+        ArrayList<Actor> actors = getActors();
+        processActors(actors);
+        //ArrayList<Location> moveLocs = getMoveLocations();
+        //Location loc = selectMoveLocation(moveLocs);
+        Location location = getLocation().getAdjacentLocation(getDirection());
+        if(end)
+        {
+			sleep++;
+		}
+		if(sleep == 5)
+		{
+			end = false;
+			if(getGrid().isValid(location))
+			{
+				Stone stone = new Stone();
+				ArrayList<Location> arrL = getGrid().getEmptyAdjacentLocations(getLocation());
+				stone.putSelfInGrid(getGrid(),arrL.get(0));
+			}
+		}
+		if(!end) 
+		{
+			if(getGrid().isValid(location))
+				makeMove(location);
+			else
+			{
+				end = true;
+				steps = 0;
+			}
+		}
+    }
+    
+    
+	public void makeMove(Location loc)
+	{
+		Actor actor = getGrid().get(loc);
+		if(actor instanceof Boulder)
+		{
+			actor.removeSelfFromGrid();
+			Kaboom kaboom = new Kaboom();
+			kaboom.putSelfInGrid(getGrid(),loc);
+			removeSelfFromGrid();
+			return;
+		}
+		else if(actor != null)
+		{
+			if(steps == 5)
+			{
+				end = true;
+				steps = 0;
+			}
+			moveTo(loc);
+			steps++;
+		}
+		else
+		{
+			end = true;
+		}
 		
 	}
 }
--- CoyoteRunner.java
+++ CoyoteRunner.java
@@ -1,1 +1,16 @@
+import info.gridworld.grid.BoundedGrid;
+import info.gridworld.grid.Location;
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.actor.Actor;
 
+public class CoyoteRunner
+{
+    public static void main(String[] args)
+    {
+        BoundedGrid<Actor> mygrid = new BoundedGrid<Actor>(20,20);
+        ActorWorld world = new ActorWorld(mygrid);
+        world.add(new Location(3,3), new Coyote());
+        world.add(new Location(7,2), new Coyote());
+        world.show();
+    }
+}
